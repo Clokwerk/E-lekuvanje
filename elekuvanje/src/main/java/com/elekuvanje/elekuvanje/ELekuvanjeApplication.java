687a507c29f9dc177785adb5c964d8a2b9dbf225
Clokwerk <35271524+Clokwerk@users.noreply.github.com>
2021-02-18 10:14:09 -0800

    Initial project files
    Initial Spring Boot project with dependencies for databases,security and Thymeleaf.

---
+++ elekuvanje/src/main/java/com/elekuvanje/elekuvanje/ELekuvanjeApplication.java
@@ -0,0 +1,13 @@
+package com.elekuvanje.elekuvanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELekuvanjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELekuvanjeApplication.class, args);
+    }
+
+}
---
+++ elekuvanje/src/test/java/com/elekuvanje/elekuvanje/ELekuvanjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elekuvanje.elekuvanje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELekuvanjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
