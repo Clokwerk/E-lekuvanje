0d2ddf2c60b6bd70455259fecd5d72991c761d77
Clokwerk <35271524+Clokwerk@users.noreply.github.com>
2021-03-01 11:52:51 -0800

    Fixed css linking.
    Fixed css linking. Only one page left to fix.

--- elekuvanje/src/main/java/com/elekuvanje/elekuvanje/web/PatientController.java
+++ elekuvanje/src/main/java/com/elekuvanje/elekuvanje/web/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -37,6 +38,11 @@ private final UserRepository userRepository;
         request.getSession().invalidate();
         return "redirect:/patient/login";
     }
+    @GetMapping(value="/register")
+    public String register(Model model){
+        return "registracija";
+    }
+
 
 
     @GetMapping(value="/termini")
